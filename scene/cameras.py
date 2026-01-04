388674dbfbd10f2010cbec25a6034ceb58064aac
hvieacl <1224185301@qq.com>
2026-01-04 17:22:09 +0800

    comment v2

--- scene/cameras.py
+++ scene/cameras.py
@@ -24,14 +24,15 @@ class Camera(nn.Module):
                  ):
         super(Camera, self).__init__()
 
-        self.uid = uid
-        self.colmap_id = colmap_id
-        self.R = R
-        self.T = T
-        self.FoVx = FoVx
-        self.FoVy = FoVy
-        self.image_name = image_name
+        self.uid = uid #相机的唯一标识符
+        self.colmap_id = colmap_id #colmap中相机位姿的ID
+        self.R = R #投影转换旋转矩阵
+        self.T = T #投影转换平移矩阵
+        self.FoVx = FoVx #相机的水平视场角
+        self.FoVy = FoVy #相机的垂直视场角
+        self.image_name = image_name #图像文件名
 
+        # 设置数据设备
         try:
             self.data_device = torch.device(data_device)
         except Exception as e:
@@ -39,24 +40,35 @@ class Camera(nn.Module):
             print(f"[Warning] Custom device {data_device} failed, fallback to default cuda device" )
             self.data_device = torch.device("cuda")
 
+        #把输入 PIL图像转成训练用的张量
         resized_image_rgb = PILtoTorch(image, resolution)
+        #取前三通道作为 RGB 真实图像（不含 alpha）
         gt_image = resized_image_rgb[:3, ...]
+
+        #alpha 掩码创建
         self.alpha_mask = None
+        #如果输入有 4 个通道（RGBA），用第 4 通道作为 self.alpha_mask 并移动到 self.data_device
         if resized_image_rgb.shape[0] == 4:
             self.alpha_mask = resized_image_rgb[3:4, ...].to(self.data_device)
+        #否则创建一个全 1 的 alpha_mask（表示完全不透明）
         else: 
             self.alpha_mask = torch.ones_like(resized_image_rgb[0:1, ...].to(self.data_device))
 
+        #train/test 暂停曝光处理
+        #当启用且这是一个“测试视图”时，会把掩码的一半置为 0，以在同一张图上分别保留训练/测试区域。
         if train_test_exp and is_test_view:
+            #如果是 is_test_dataset（表示整个图像集合属于测试集），则把掩码的左半边设为 0；否则把右半边设为 0。
             if is_test_dataset:
                 self.alpha_mask[..., :self.alpha_mask.shape[-1] // 2] = 0
             else:
                 self.alpha_mask[..., self.alpha_mask.shape[-1] // 2:] = 0
 
+        #把真实图像归一化压缩后移动到 self.data_device，并保存图像宽高
         self.original_image = gt_image.clamp(0.0, 1.0).to(self.data_device)
         self.image_width = self.original_image.shape[2]
         self.image_height = self.original_image.shape[1]
 
+        #处理逆深度图，将其缩放/清理并转换为用于训练的 PyTorch 张量，同时生成一个像素级的可靠性掩码。
         self.invdepthmap = None
         self.depth_reliable = False
         if invdepthmap is not None:
@@ -77,17 +89,21 @@ class Camera(nn.Module):
                 self.invdepthmap = self.invdepthmap[..., 0]
             self.invdepthmap = torch.from_numpy(self.invdepthmap[None]).to(self.data_device)
 
+        #相机视角的最近点和最远点
         self.zfar = 100.0
         self.znear = 0.01
 
         self.trans = trans
         self.scale = scale
 
+        #计算相机的世界到视图变换矩阵、投影矩阵和完整的投影变换矩阵
+        #CG相关变换过程
         self.world_view_transform = torch.tensor(getWorld2View2(R, T, trans, scale)).transpose(0, 1).cuda()
         self.projection_matrix = getProjectionMatrix(znear=self.znear, zfar=self.zfar, fovX=self.FoVx, fovY=self.FoVy).transpose(0,1).cuda()
         self.full_proj_transform = (self.world_view_transform.unsqueeze(0).bmm(self.projection_matrix.unsqueeze(0))).squeeze(0)
         self.camera_center = self.world_view_transform.inverse()[3, :3]
         
+#简化版相机类，只保存必要的相机参数
 class MiniCam:
     def __init__(self, width, height, fovy, fovx, znear, zfar, world_view_transform, full_proj_transform):
         self.image_width = width
--- utils/graphics_utils.py
+++ utils/graphics_utils.py
@@ -28,6 +28,7 @@ def geom_transform_points(points, transf_matrix):
     denom = points_out[..., 3:] + 0.0000001
     return (points_out[..., :3] / denom).squeeze(dim=0)
 
+#构造并返回一个 4x4 的 world→view 变换矩阵（Rt）
 def getWorld2View(R, t):
     Rt = np.zeros((4, 4))
     Rt[:3, :3] = R.transpose()
@@ -35,19 +36,26 @@ def getWorld2View(R, t):
     Rt[3, 3] = 1.0
     return np.float32(Rt)
 
+#CG之视图变换
+#考虑相机中心变换的情况下，构造并返回一个 4x4 的 world→view 变换矩阵（Rt）
+#先将相机中心在世界坐标下平移并缩放后再反算回新的 world→view，使相机中心受 translate/scale 影响。
 def getWorld2View2(R, t, translate=np.array([.0, .0, .0]), scale=1.0):
     Rt = np.zeros((4, 4))
     Rt[:3, :3] = R.transpose()
     Rt[:3, 3] = t
     Rt[3, 3] = 1.0
 
-    C2W = np.linalg.inv(Rt)
-    cam_center = C2W[:3, 3]
-    cam_center = (cam_center + translate) * scale
-    C2W[:3, 3] = cam_center
-    Rt = np.linalg.inv(C2W)
+    C2W = np.linalg.inv(Rt)  # 把 Rt 视作 world→view 的逆（得到 camera→world）
+    cam_center = C2W[:3, 3]  # 提取相机中心（world 坐标）
+    cam_center = (cam_center + translate) * scale  # 对相机中心应用平移与缩放
+    C2W[:3, 3] = cam_center  # 把修改后的相机中心写回
+    Rt = np.linalg.inv(C2W)  # 重新求逆得到新的 world→view
     return np.float32(Rt)
 
+#CG之投影变换
+#构造并返回一个 4x4 的 projection 矩阵（P）
+#用于将视图空间点投影到裁剪空间（NDC），基于给定近/远裁剪面和水平/垂直视野角。
+#这里是为了线性化方便反向传播 以及保留深度信息 确定渲染顺序
 def getProjectionMatrix(znear, zfar, fovX, fovY):
     tanHalfFovY = math.tan((fovY / 2))
     tanHalfFovX = math.tan((fovX / 2))
